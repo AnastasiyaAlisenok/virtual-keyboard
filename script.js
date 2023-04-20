1080ffc62d7791abf6ffacadaf7174b0ac8f3b6d
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-04-20 17:49:10 +0300

    feat: add backspace, caps, enter, space and default buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Ряды для создания клавиатуры
 
-const ROWS = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',"TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", 'DEL', 'CAPS LOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'ENTER', 'SHIFT', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '0', 'SHIFT', 'CTRL', 'WIN', 'ALT', '', 'ALT', 'CTRL', '', '', ''];
+const ROWS = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',"TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", 'DEL', 'CAPS LOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'ENTER', 'SHIFT', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '', 'SHIFT', 'CTRL', 'WIN', 'ALT', 'SPACE', 'ALT', 'CTRL', '', '', ''];
 const ROW1_TOP = [ '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', ''];
 
 const body = document.querySelector('body');
@@ -12,59 +12,108 @@ const keyboard = {
     rows: null,
     button: null
   },
+  properties: {
+    value: '',
+    capsLock: false
+  },
 
   init() {
     this.elements.textarea = document.createElement('textarea');
-    this.elements.textarea.rows = 15;
+    this.elements.textarea.rows = 10;
     this.elements.textarea.className = 'text';
     body.append(this.elements.textarea);
 
     this.elements.main = document.createElement('div');
     this.elements.main.className = 'keyboard';
     body.append(this.elements.main);
-
+    
     this.elements.rows = document.createElement('div');
     this.elements.rows.className = 'row';
     this.elements.main.append(this.elements.rows);
+
   },
 
   createButtons() {
     for(let i=0; i<ROWS.length; i++) {
-      this.elements.button = document.createElement('button');
+      const button = document.createElement('button');
       if(i>=0 && i<=12) {
         const topLetter = document.createElement('div');
         const centerLetter = document.createElement('div');
         topLetter.className = 'row1__top_letter';
         topLetter.textContent = ROW1_TOP[i];
         centerLetter.textContent = ROWS[i];
-        this.elements.button.append(topLetter);
-        this.elements.button.append(centerLetter);
-        this.elements.button.className = 'row__letter';
+        button.append(topLetter);
+        button.append(centerLetter);
+        button.className = 'row__letter';
       } else if( i === 54) {
-        this.elements.button.className = 'row__letter';
-        this.elements.button.classList.add('up');
+        button.className = 'row__letter';
+        button.classList.add('up');
       } else if(i === 62) {
-        this.elements.button.className = 'row__letter';
-        this.elements.button.classList.add('left');
+        button.className = 'row__letter';
+        button.classList.add('left');
       } else if(i === 63) {
-        this.elements.button.className = 'row__letter';
-        this.elements.button.classList.add('down');
+        button.className = 'row__letter';
+        button.classList.add('down');
       } else if(i === 64) {
-        this.elements.button.className = 'row__letter';
-        this.elements.button.classList.add('right');
+        button.className = 'row__letter';
+        button.classList.add('right');
       } else {
-        this.elements.button.textContent = ROWS[i];
-        this.elements.button.className = 'row__letter';
+        button.textContent = ROWS[i];
+        button.className = 'row__letter';
       }
       if(i >= 56) {
-        this.elements.button.classList.add('row5__letter');
+        button.classList.add('row5__letter');
       }
-      this.elements.rows.append(this.elements.button);
+      switch(button.textContent) {
+        case 'Backspace': 
+          button.addEventListener('click', () => {
+            this.properties.value = this.properties.value.substring(0, this.properties.value.length-1);
+            this.elements.textarea.textContent = this.properties.value;
+          })
+          break;
+    
+          case 'CAPS LOCK':
+          button.addEventListener('click', () => {
+            button.classList.toggle(this.properties.capsLock);
+            this.toggleCapsLock();
+          })
+          break;
+    
+          case 'SPACE':
+          button.addEventListener('click', () => {
+            this.properties.value += ` `;
+            this.elements.textarea.textContent = this.properties.value;
+          })
+          break;
+
+          case 'ENTER':
+          button.addEventListener('click', () => {
+            this.properties.value += '\n';
+            this.elements.textarea.textContent = this.properties.value;
+          })
+          break;
+    
+          default:
+          button.addEventListener('click', () => {
+            this.properties.value += this.properties.capsLock ? button.textContent.toUpperCase() : button.textContent.toLowerCase();
+            this.elements.textarea.textContent = this.properties.value;
+          })
+          break;
+      }
+      this.elements.button = this.elements.rows.querySelectorAll('.row__letter');
+      this.elements.rows.append(button);
   }
-  }
+  },
+  toggleCapsLock() {
+      this.properties.capsLock = ! this.properties.capsLock;
+  },
+
+ 
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   keyboard.init()
   keyboard.createButtons()
 })
+
+console.log(keyboard)
