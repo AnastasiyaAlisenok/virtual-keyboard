853e31f3abb7746bbe2adcd7b309262194c16ceb
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-04-28 00:05:30 +0300

    feat: add button shift

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const body = document.querySelector('body');
 class Keyboard {
   constructor() {
     this.ROWS = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL', 'CAPS LOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'ENTER', 'SHIFT', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '', 'SHIFT', 'CTRL', 'WIN', 'ALT', 'SPACE', 'ALT', '', '', '', 'CTRL'];
+    this.ROWS_SHIFT = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'DEL', 'CAPS LOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'ENTER', 'SHIFT', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '', 'SHIFT', 'CTRL', 'WIN', 'ALT', 'SPACE', 'ALT', '', '', '', 'CTRL'];
+    this.rusRows = ['ё', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.'];
     this.textarea = null;
     this.main = null;
     this.rows = null;
@@ -24,7 +26,9 @@ class Keyboard {
 
     this.rows = document.createElement('div');
     this.rows.className = 'row';
+  }
 
+  addButtons() {
     for (let i = 0; i < this.ROWS.length; i += 1) {
       const button = document.createElement('button');
       if (i === 13 || i === 14 || i === 28 || i === 29
@@ -55,13 +59,18 @@ class Keyboard {
       switch (button.textContent) {
         case 'Backspace':
           button.addEventListener('click', () => {
+            button.classList.add('active');
             this.value = this.value.substring(0, this.value.length - 1);
             this.textarea.value = this.value;
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
           });
           break;
 
         case 'CAPS LOCK':
           button.addEventListener('click', () => {
+            button.classList.add('active');
             this.toggleCapsLock();
             button.classList.toggle('caps', this.capsLock);
           });
@@ -69,51 +78,103 @@ class Keyboard {
 
         case 'SPACE':
           button.addEventListener('click', () => {
+            button.classList.add('active');
             this.value += ' ';
             this.textarea.value = this.value;
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
           });
           break;
 
         case 'ENTER':
           button.addEventListener('click', () => {
+            button.classList.add('active');
             this.value += '\n';
             this.textarea.value = this.value;
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
           });
           break;
 
         case 'TAB':
           button.addEventListener('click', () => {
+            button.classList.add('active');
             this.value += '    ';
             this.textarea.value = this.value;
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
           });
           break;
 
         case 'DEL':
           button.addEventListener('click', () => {
+            button.classList.add('active');
             this.value = this.textarea.value.slice(0, this.textarea.selectionStart)
             + this.textarea.value
               .slice(this.textarea.selectionStart + 1, this.textarea.value.length);
             this.textarea.value = this.value;
           });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
+          });
           break;
 
         case 'CTRL':
-          button.addEventListener('click', () => { });
+          button.addEventListener('click', () => {
+            button.classList.add('active');
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
+          });
           break;
 
         case 'SHIFT':
-          button.addEventListener('click', () => { });
+          button.addEventListener('mousedown', () => {
+            button.classList.add('active-shift');
+            const buttons = document.querySelectorAll('.row__letter');
+            for (let j = 0; j < buttons.length; j += 1) {
+              buttons[j].textContent = this.ROWS_SHIFT[j];
+            }
+          });
+          button.addEventListener('mouseup', () => {
+            button.classList.remove('active-shift');
+            const buttons = document.querySelectorAll('.row__letter');
+            for (let j = 0; j < buttons.length; j += 1) {
+              if (j === 13 || j === 14 || j === 28 || j === 29
+                || j === 41 || j === 42 || j === 63 || (j >= 54 && j <= 59)) {
+                buttons[j].textContent = this.ROWS[j];
+              } else {
+                buttons[j].textContent = this.ROWS[j].toLowerCase();
+              }
+            }
+          });
           break;
 
         case 'ALT':
-          button.addEventListener('click', () => { });
+          button.addEventListener('click', () => {
+            button.classList.add('active');
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
+          });
           break;
 
         default:
-          button.addEventListener('click', () => {
-            this.value += this.capsLock ? button.textContent.toUpperCase()
-              : button.textContent.toLowerCase();
+          button.addEventListener('click', (event) => {
+            button.classList.add('active');
+            if (event.shiftKey) {
+              this.value += button.textContent;
+            } else {
+              this.value += this.capsLock ? button.textContent.toUpperCase()
+                : button.textContent.toLowerCase();
+            }
             this.textarea.value = this.value;
+          });
+          button.addEventListener('animationend', () => {
+            button.classList.remove('active');
           });
           break;
       }
@@ -140,7 +201,7 @@ class Keyboard {
       event.preventDefault();
       const buttons = document.querySelectorAll('.row__letter');
       const letter = Array.from(buttons).find((item) => item.innerHTML === event.key.toLowerCase()
-      || item.innerHTML === event.key || item.innerHTML.toLowerCase() === event.key);
+      || item.textContent === event.key || item.innerHTML.toLowerCase() === event.key);
 
       if (event.key === 'Backspace') {
         this.value = this.value.substring(0, this.value.length - 1);
@@ -175,14 +236,16 @@ class Keyboard {
       } else if (event.code === 'ShiftLeft') {
         for (let i = 0; i < buttons.length; i += 1) {
           if (i === 42 && buttons[i].textContent === 'SHIFT') {
-            buttons[i].classList.add('active');
+            buttons[i].classList.add('active-shift');
           }
+          buttons[i].textContent = this.ROWS_SHIFT[i];
         }
       } else if (event.code === 'ShiftRight') {
         for (let i = 0; i < buttons.length; i += 1) {
           if (i === 54 && buttons[i].textContent === 'SHIFT') {
-            buttons[i].classList.add('active');
+            buttons[i].classList.add('active-shift');
           }
+          buttons[i].textContent = this.ROWS_SHIFT[i];
         }
       } else if (event.code === 'Space') {
         this.value += ' ';
@@ -207,6 +270,11 @@ class Keyboard {
         } else {
           buttons.forEach((item) => (item.textContent === event.key.toLowerCase() ? item.classList.add('active') : 0));
         }
+        if (event.shiftKey) {
+          buttons.forEach((item) => (item.textContent === event.key
+            || item.textContent.toLowerCase() === event.key
+            ? item.classList.add('active') : 0));
+        }
         this.value += letter.textContent;
       }
       this.textarea.value = this.value;
@@ -214,12 +282,46 @@ class Keyboard {
         item.classList.remove('active');
       }, false));
     });
+    document.addEventListener('keyup', (event) => {
+      const buttons = document.querySelectorAll('.row__letter');
+
+      if (event.key === 'Shift') {
+        for (let i = 0; i < buttons.length; i += 1) {
+          if (buttons[i].textContent === 'SHIFT') {
+            buttons[i].classList.remove('active-shift');
+          }
+          if (i === 13 || i === 14 || i === 28 || i === 29
+            || i === 41 || i === 42 || i === 63 || (i >= 54 && i <= 59)) {
+            buttons[i].textContent = this.ROWS[i];
+          } else {
+            buttons[i].textContent = this.ROWS[i].toLowerCase();
+          }
+        }
+      }
+    });
+  }
+
+  switchLanguage() {
+    const letters = document.querySelectorAll('.letter');
+
+    for (let i = 0; i < letters.length; i += 1) {
+      if ((i < 1 || i > 12) && i !== 25) {
+        letters[i].classList.add('translate');
+      }
+    }
+
+    const lettersTranslate = document.querySelectorAll('.translate');
+
+    for (let i = 0; i < lettersTranslate.length; i += 1) {
+      lettersTranslate[i].textContent = this.rusRows[i];
+    }
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   const keyboard = new Keyboard();
   keyboard.init();
+  keyboard.addButtons();
   keyboard.clickPhisicalKeyboard();
   document.querySelector('.keyboard').addEventListener('click', () => {
     document.querySelector('.text').focus();
